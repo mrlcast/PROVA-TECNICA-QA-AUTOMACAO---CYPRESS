42a5638f5e059b512ce96458d07cc1a4af9979a3
mrlcast <marlon.castilho.almeida@gmail.com>
2022-04-16 10:37:44 -0300

    Adicionando validações e excluindo pastas inutilizadas

--- cypress/integration/Produto no carrinho.spec.js
+++ cypress/integration/Produto no carrinho.spec.js
@@ -1,29 +1,33 @@
+//Aqui é importado o base, onde constam as regras do negócio
 import Base from '../pages/Base'
 
-describe('Adicionando produto no carrinho e validando o mesmo', ()=>{
-    
+//Essa é a descrição do objetivo do código
+describe('Adicionando produto no carrinho e validando o mesmo', ()=>{  
+    //Trecho para evitar que erros fora do contexto falhem o teste
     Cypress.on('uncaught:exception', (err, runnable) => {
         // returning false here prevents Cypress from
         // failing the test
         return false
         })
     
-    it('Usuario acessa o ecommerce', function(){
+    //As regras de negócio estão encapsuladas no Base, que será chamado em cada uma das suítes de testes
+    //Acesso do usuário ao e-commerce Amazon
+    it('Usuario acessa o e-commerce', function(){
         Base.go()   
     })
-
+    //Pesquisa do usuário pelo produto escolhido
     it('Usuário pesquisa o produto', function(){
         Base.search()
     })
-
+    //Seleção do produto e tamanho
     it('Usuário seleciona o produto e o tamanho', function(){
          Base.select()
     })
-
+    //Adicionando o produto ao carrinho e indo à página do carrinho
     it('Usuário adiciona o produto ao carrinho',function(){
         Base.addToCart()
     })
-
+    //Validações dos dados do produto selecionado
     it('Usuário valida os dados do carrinho',function(){
         Base.asserts()
     })
--- cypress/pages/Base.js
+++ cypress/pages/Base.js
@@ -1,23 +1,26 @@
+//As chamadas da classe de testes (spec.js) referenciam o base abaixo, onde constam os códigos do caminho dos testes do usuário
 class Base {
+    //Aqui o código manda um comando para visitar a URL base, que está apontada no cypress.json
     go(){
         cy.visit('/')
     }
+    //Aqui o código digita o nome do produto e clica em pesquisar
     search() {
         cy.get('input[name="field-keywords"]').type("tenis adidas")
         cy.get('input[id="nav-search-submit-button"]').click()
     }
-
+    //Aqui o código seleciona o produto e o tamanho dele
     select(){
         cy.get('[data-asin="B07ZHXCZPT"] > .sg-col-inner > .s-widget-container > [data-component-type="s-impression-logger"] > .s-featured-result-item > .s-card-container > .a-spacing-base > .a-spacing-small > .s-title-instructions-style > .a-size-mini > .a-link-normal > .a-size-base-plus').click()    
         cy.get('#native_dropdown_selected_size_name').select('12')
     }    
-    
+    //Aqui o código adiciona ao carrinho e entra no carrinho
     addToCart(){
         cy.wait(2000)
         cy.get('#add-to-cart-button').click()
         cy.get('#sw-gtc > .a-button-inner > .a-button-text').click()
     }
-
+    //Aqui o código faz os asserts dos dados do produto
     asserts(){
         cy.get('h1').should('have.text', '\nCarrinho de compras\n')
         cy.get('.a-truncate-cut').should('have.text', 'adidas Originals Tênis masculino Top Ten, Branco/Giz Branco/Branco, 12')
@@ -26,5 +29,5 @@ class Base {
 
     }
 }
-
+//Aqui é exportado o base, que será referenciado 
 export default new Base;
